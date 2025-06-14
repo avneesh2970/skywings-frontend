fbe0da6bd2dd1e3686cbc73a5566ac40979c5429
Avneesh Kumar <95642991+avneesh2970@users.noreply.github.com>
2025-06-14 19:54:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-// import { Outlet } from "react-router-dom";
-// import "./App.css";
-// import { ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import Navbar from "./componentes/navbar";
-// import Footer from "./componentes/footer";
-// import CookieBanner from "./componentes/CookieBanner";
-// import SocialmediaConn from "./componentes/SocialmediaConn";
-// import { Toaster } from "react-hot-toast";
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "./componentes/navbar";
+import Footer from "./componentes/footer";
+import CookieBanner from "./componentes/CookieBanner";
+import SocialmediaConn from "./componentes/SocialmediaConn";
+import { Toaster } from "react-hot-toast";
 
-// function App() {
-//   return (
-//     <>
-//       <CookieBanner />
-//       <SocialmediaConn />
-//       <Navbar />
-//       <Outlet />
-//       <Footer />
-//       <Toaster />
-//       <ToastContainer position="top-right" autoClose={3000} />
-//     </>
-//   );
-// }
+function App() {
+  return (
+    <>
+      <CookieBanner />
+      <SocialmediaConn />
+      <Navbar />
+      <Outlet />
+      <Footer />
+      <Toaster />
+      <ToastContainer position="top-right" autoClose={3000} />
+    </>
+  );
+}
 
-// export default App;
+export default App;
